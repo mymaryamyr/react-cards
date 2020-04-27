def5a0d426606b1502d7a8f0ba9e6ffe00690c90
mymaryamyr <mymaryamyr@gmail.com>
2020-04-27 11:30:38 +0430

    Updates

---
+++ precache-manifest.33f8b50947abc62dfc15c19be18c30f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feec9b14e8e173bbec98306aa098110a",
+    "url": "/react-cards/index.html"
+  },
+  {
+    "revision": "452dbd86d791c28853bd",
+    "url": "/react-cards/static/css/main.886470db.chunk.css"
+  },
+  {
+    "revision": "cb0f8881cb168c5395d4",
+    "url": "/react-cards/static/js/2.3edaf8cb.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/react-cards/static/js/2.3edaf8cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "452dbd86d791c28853bd",
+    "url": "/react-cards/static/js/main.c3808904.chunk.js"
+  },
+  {
+    "revision": "803c2b2e69e8d4f0b4c6",
+    "url": "/react-cards/static/js/runtime-main.5fcf487e.js"
+  },
+  {
+    "revision": "4f68900b2f43102d5f5a6e276091412d",
+    "url": "/react-cards/static/media/safron.4f68900b.png"
+  }
+]);
